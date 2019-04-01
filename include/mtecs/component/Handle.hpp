7bbcbfe55926004d9e429ef6692b2c1a2d65f78f
Gabriel <gabrielmuttiteixeira@gmail.com>
2019-04-01 08:03:17 -0300

    Added handle validation and invalidation

--- include/mtecs/component/Handle.hpp
+++ include/mtecs/component/Handle.hpp
@@ -8,28 +8,32 @@ namespace mtecs::internal
     class Handle
     {
     private:
-	IPool& pool;
-	uint index;
+		IPool& pool;
+		uint index;
+		bool isValid;
 	
     public:
-	Handle(IPool& pool, uint index);
+		Handle(IPool& pool, uint index);
 	
-	inline void* get();
+		inline void* get();
+		
+		bool valid();
+		void invalidate();
 
-	inline bool operator ==(Handle& other) const
-	{
-	    return (index == other.index);
-	}
+		inline bool operator ==(Handle& other) const
+		{
+			return index == other.index;
+		}
 
-	inline bool operator !=(Handle& other) const
-	{
-	    return (index != other.index);
-	}
+		inline bool operator !=(Handle& other) const
+		{
+			return index != other.index;
+		}
 
-	inline void operator=(const Handle& other)
-	{
-	    pool = other.pool;
-	    index = other.index;
-	}
+		inline void operator=(const Handle& other)
+		{
+			pool = other.pool;
+			index = other.index;
+		}
     };
 }
